47dfb45704a1e125e8be5f813a789c2892322616
johnny <johnny@moodring.local>
2020-03-11 19:58:07 -0500

    ver.2020-03-11

--- zombiedice.go
+++ zombiedice.go
@@ -1,177 +1,130 @@
 // name:    zombieDice
 // author:  johnny
-// version: 2018/10/09 19:30.  Complete rewrite
-// stds:    space delimited
-// notes:   actual gameplay still not done.
-//          currently, only completed logic for rolls, die, color, face, and scoring
-//          !!!!!	next step.. convert to prepopulated roll var to a slice
-//          and remove an element when it becomes brain or shotgun
-//          also.. adjust the min/max of the slice dynamically for the for/next to work properly
-// urls:    https://github.com/logrusorgru/aurora
+// version: 2018/10/09 19:30 complete rewrite
+// notes: actual gameplay still not done.
+//   currently, only completed logic for rolls, die, color, face, and scoring
+//   !!!!! next step.. convert to prepopulated roll var to a slice
+//     and remove an element when it becomes brain or shotgun
+//     also.. adjust the min/max of the slice dynamically for the for/next to work properly
 package main
 
 import "fmt"
 import "math/rand"                                                     // for random numbers
 import "time"                                                          // for random seed
-import . "github.com/logrusorgru/aurora"                               // for ANSI color output.. url: https://godoc.org/github.com/fatih/color
 
-// VARS GLOBAL ========================================================
-//   CONSTANTS --------------------------------------------------------
-const brain int = 0                                                   // index for brain
-const shotgun int = 1                                                 // index for shotgun
-const runner int = 2                                                  // index for runner
-const green int = 0                                                   // index for green    fyi - (6 green die)
-const yellow int = 1                                                  // index for yellow   fyi - (4 yellow die)
-const red int = 2                                                     // index for red      fyi - (3 red die)
-const white int = 99                                                  // ANSI color white - arbitrary value
-const cupMax int = 3                                                  // how many dice can the cup hold
+//import "os"
 
+// CONSTANTS -----------------------------------------------------------
+const brain int = 0                                                    // index for brain
+const shotgun int = 1                                                  // index for shotgun
+const runner int = 2                                                   // index for runner
+const green int = 0                                                    // index for green
+const yellow int = 1                                                   // index for yellow
+const red int = 2                                                      // index for red
+const totalNumberOfDice = 13                                           // total # of die
 
-func getDieColor(x int) Color {
-  if x == green {
-    return GreenFg              //|BlackBg
-  } else if x == red {
-    return RedFg                //|BlackBg
-  } else if x == yellow {
-    return BrownFg              //|BlackBg
-  } else if x == white {
-    return BlackFg               //|BlackBg
-  } else {
-    return BlackFg               //|BlackBg
-  }
+// VARS ----------------------------------------------------------------
+var x int        // misc var
+var dieColor int      // color picked from cup during the setup
+var diceColorQuantity [3]int
+var dieColorName []string                                              // die color description
+var dieFace []string                                                   // die face description
+// FUNCTIONS ===========================================================
+func printSlice(z int, s []int) {
+  fmt.Printf("z=%d :  len=%d    %#v      \n", z, len(s), s)
 }
 
+func randomizeDiceInCup(howManyDice int) (cup []int) {
+  x=0
+  for x < howManyDice {
+    dieColor = rand.Intn(3)
+    if diceColorQuantity[dieColor] > 0 {                                      // if there is still THIS-COLOR remaining
+printSlice(x, cup)
+      cup=append(cup,dieColor)                                                // prepoulate
+      x+=1                                                                //
+      diceColorQuantity[dieColor]-=1                                           // reduce THIS-COLOR by one
+    }
+  }
+  return
+}
 
-
-// MAIN	===============================================================
+// MAIN ================================================================
 func main() {
 
-// VARS  ==============================================================
-// informational vars
-  var dieColorName [3]string                                            // color names for the dice.  (human readable)
-  dieColorName[green] = "Green"
-  dieColorName[yellow] = "Yellow"
-  dieColorName[red] = "Red"
+// VARS ----------------------------------------------------------------
+//   informational vars
+//
+  dieColorName=append(dieColorName,"Green")                            // 0  NOTE: These appends HAVE TO BE IN THIS ORDER
+  dieColorName=append(dieColorName,"Yellow")                           // 1        so that they match up with the CONSTANTS above
+  dieColorName=append(dieColorName,"Red")                              // 2        i.e. green=0 .. append(green) needs to be 0 index
 
-  var dieFace [3]string                                                // face names for dice.  (human readable)
-  dieFace[brain] =   "Brain   "
-  dieFace[runner] =  "runner  "                                        // NOTE: Padded spaces to end in order to add ANSI later
-  dieFace[shotgun] = "SHOTGUN "                                        //       couldnt combine Colorize with Sprintf %-6s
+  dieFace=append(dieFace,"Brain")                                      // 0  NOTE: These appends HAVE TO BE IN THIS ORDER
+  dieFace=append(dieFace,"Runner")                                     // 1        so that they match up with the CONSTANTS above
+  dieFace=append(dieFace,"SHOTGUN")                                    // 2        i.e. brain=0 .. append(brain) needs to be 0 index
 
-  var diceQuantity [3]int                                              // array of the different die quantities
-  diceQuantity[green] = 6                                              // there are six green die
-  diceQuantity[yellow] = 4                                             // there are four yellow die
-  diceQuantity[red] = 3                                                // there are three red die
-
-  //  dieSides := [][]int{}
-  var dieSides [][]int                                                 // array of all the color-icons.  sorted by color, icon.
-  greenDieSides := []int{brain, brain, brain, runner, runner, shotgun}
-  yellowDieSides := []int{brain, brain, runner, runner, shotgun, shotgun}
-  redDieSides := []int{brain, runner, runner, shotgun, shotgun, shotgun}
-  dieSides = append(dieSides, greenDieSides)                           // populate greens first.   (NOTE: i used slice to allow utilize the append function / readability)
-  dieSides = append(dieSides, yellowDieSides)                          //   then yellows
-  dieSides = append(dieSides, redDieSides)                             //   finally reds
-
-// game prep vars
-  var dieAvailable int                                                 // how many dice in cup
-  var dieOutOfPlay int                                                 // how many dice (shotguns/brains) that are now out of play
-  var dieOrder []int                                                   // the prepopulated random order of dice pulled from the bag (For a slice, DONT put size) !!This is a slice
-  var dieColor int                                                     // color picked from cup during the setup
-  var dice [3]int                                                      // three dice in cup for rolling..
-  var roll int                                                         // index for the three dice you are rolling  (0-2)
-  var rollResult int                                                   // the result of a rolled die (ie side facing up)
-  var runningScore int                                                 // the running score of this round
-  var finalScore int                                                   // the total score of the game
-
-//  normal vars
-  var x int                                                            // misc var
-  var nextDiceAvail int = 0                                            // index of diceAvail/slice position for drawing 3 dice into cup
-
-  var score [3]int
-  score[brain] = 0                                                     // how many brains can you eat ;-)
-  score[shotgun] = 0                                                   // just for stats.. doesnt affect game outcome
-  score[runner] = 0                                                    // max shotgun hits you can receive
+ greenDieSides := []int{brain, brain, brain, runner, runner, shotgun}
+ yellowDieSides := []int{brain, brain, runner, runner, shotgun, shotgun}
+ redDieSides := []int{brain, runner, runner, shotgun, shotgun, shotgun}
+ dieSides := [][]int{}                                                 // one array listing all possible sides for each colored dice
+ dieSides = append(dieSides, greenDieSides)                            // 0. since dieSides is NULL/just initiated.. the first index will be 0.
+ dieSides = append(dieSides, yellowDieSides)                           // 1
+ dieSides = append(dieSides, redDieSides)                              // 2
 
 
-//	INIT	=========================================================
-//delthis. left here to remember about len and cap capabilities
-//fmt.Println("len of dieOrder: ", len(dieOrder), ", ", cap(dieOrder))
+//   game prep vars
+//
+ var numDieInCup int                                                   // how many dice in cup
+ var dieOutOfPlay int                                                  // how many dice (shotguns/brains) that are now out of play
+// dieInCup := make([]int, 13)                                           // the prepopulated random order of dice pulled from the bag
+ dieInCup := []int{}                                           // the prepopulated random order of dice pulled from the bag
+ 
+ diceColorQuantity[green] = 6     // there are initially six green die in the bag
+ diceColorQuantity[yellow] = 4    // there are initially four yellow die in the bag
+ diceColorQuantity[red] = 3     // there are initially three red die in the bag
 
-  rand.Seed(time.Now().UnixNano())
-  dieAvailable = 13
-  dieOutOfPlay = 13 - dieAvailable
+//   normal vars
+//
+ var y int        // misc var
+ var rolld6 int
 
-  // prepopulate the random dice order ie. the order that dice will be pulled from the available pool
-  //   i.e. randomly picking the available colored dice and putting them in the diOrder slice/array.
-  x=0
-  for x < dieAvailable {
-    dieColor = rand.Intn(3)
-    if diceQuantity[dieColor] > 0 {                                    // if there is still THIS-COLOR remaining
-      dieOrder=append(dieOrder, dieColor)                                             // prepoulate
-      x+=1                                                             //
-      diceQuantity[dieColor]-=1                                        // reduce THIS-COLOR by one
-    }
+ var score [3]int
+ score[brain] = 0   // how many brains can you eat ;-)
+ score[shotgun] = 0  // just for stats.. doesnt affect game outcome
+ score[runner] = 0 // max shotgun hits you can receive
+
+
+// INIT =========================================================
+ numDieInCup = len(dieInCup)
+ dieOutOfPlay = totalNumberOfDice - numDieInCup
+ rand.Seed(time.Now().UnixNano())
+
+ // titles
+ fmt.Println("\n\nZombie Dice")
+ fmt.Println("=============================================================")
+  var myCup []int
+  printSlice(98, myCup)
+ // prepopulate the random dice order ie. the order that dice will be pulled from the cup
+  myCup = randomizeDiceInCup(totalNumberOfDice)
+  printSlice(99, myCup)
+
+
+
+
+ // MAIN =========================================================
+ y = 0
+ for y < numDieInCup {
+  rolld6 = rand.Intn(6)      // roll me a die
+  score[dieSides[dieInCup[y]][rolld6]]+=1  // change the current-round score for this particular die roll
+  fmt.Printf("die %2d - %-6s %-7s   score (brains:%2d  shotguns:%2d  runners:%2d    numDieInCup: %2d , outOfPlay: %2d)\n", y+1, dieColorName[dieInCup[y]], dieFace[dieSides[dieInCup[y]][rolld6]], score[brain], score[shotgun], score[runner], numDieInCup, dieOutOfPlay )
+  y+=1
+  if score[shotgun] >= 3 {
+   fmt.Println("You have been DESTROYED!")
+   fmt.Printf("\tyou had (brains:%2d  shotguns:%2d  runners:%2d)\n", score[brain], score[shotgun], score[runner] )
+   y=99
   }
-
-
-  // titles
-  fmt.Println(Colorize("===================================================================================================", getDieColor(white)))
-  fmt.Println(Colorize("== Zombie Dice                                                                                   ==", getDieColor(white)))
-  fmt.Println(Colorize("===================================================================================================", getDieColor(white)))
-
-  finalScore=0
-  runningScore=0
-  roll=0
-  for roll < 3 {                                                       // (effectively) roll your 3 dice
-    rollResult = dieSides[dieOrder[0]][rand.Intn(6)]                   //   randomly assign value to the roll
-    score[rollResult]+=1                                               //   increase the tally for the type of die rolled
-//probably need to reduce dieAvailable and update dieOutOfPlay  somewhere in here
-
-    dice[roll] = dieOrder[0]                                           //   keep track of 3 dice
-
-    copy(dieOrder[0:], dieOrder[1:])                                   //   remove first die from the dieOrder, hence eventually rolling die 2 and 3
-    dieOrder[len(dieOrder)-1] = 0                                      //   erase last element (write to zero)
-    dieOrder = dieOrder[:len(dieOrder)-1]                              //   truncate (end of) slice, effectively reducing size of slice and making position 0 the next die in line
-
-    if rollResult == brain {
-      runningScore+=1
-    }
-    if score[shotgun] == 3 {
-//      fmt.Println("OUCH!  You received ", score[shotgun], " shotgun blasts!   At the time, you had ", runningScore, " points.")
-//works kinda      fmt.Println(Gray("OUCH!  You received ").BgBlack(), Gray(score[shotgun]).BgBlack(), Gray(" shotgun blasts!   At the time, you had ").BgBlack(), Gray(runningScore).BgBlack(), Gray(" points.").BgBlack())
-      fmt.Print(Colorize("OUCH!  You received THREE shotgun blasts!  ", getDieColor(red)))
-      fmt.Print(Colorize("At the time, you had ", getDieColor(white)))
-      fmt.Print(Colorize(runningScore, getDieColor(white)))
-      fmt.Print(Colorize(" points.", getDieColor(white)))
-      fmt.Println(Colorize("                          ", getDieColor(white)))
-      runningScore=0
-    }
-    fmt.Print(Colorize("die:", getDieColor(white)))
-    fmt.Print(Colorize(roll+1, getDieColor(white)))
-    fmt.Print(Colorize("  ", getDieColor(white)))
-    fmt.Print(Colorize(dieFace[rollResult],getDieColor(dice[roll])))       // print ANSI color of the name-color of the current (1of3) die roll
-    fmt.Print(Colorize("stats(brains:", getDieColor(green)))
-    fmt.Print(Colorize(score[brain], getDieColor(green)))
-    fmt.Print(Colorize(" shotgun:", getDieColor(red)))
-    fmt.Print(Colorize(score[shotgun], getDieColor(red)))
-    fmt.Print(Colorize(" runner:", getDieColor(yellow)))
-    fmt.Print(Colorize(score[runner], getDieColor(yellow)))
-    fmt.Print(Colorize("  dieAvailable:", getDieColor(white)))
-    fmt.Print(Colorize(dieAvailable - nextDiceAvail, getDieColor(white)))
-    fmt.Print(Colorize(" outOfPlay:", getDieColor(white)))
-    fmt.Print(Colorize(dieOutOfPlay, getDieColor(white)))
-    fmt.Println(")")
-    roll+=1
-  }
-  finalScore+=runningScore
-
-  fmt.Print(Colorize("Running score: ", getDieColor(white)))           // dieColor(white) is standard GrayFg on BlackBg
-  fmt.Print(Colorize(runningScore, getDieColor(white)))
-  fmt.Print(Colorize(".  Final score: ", getDieColor(white)))             // dieColor(white) is standard GrayFg on BlackBg
-  fmt.Print(Colorize(finalScore, getDieColor(white)))
-  fmt.Println(Colorize("                                                                                     ", getDieColor(white)))
-  fmt.Println(Colorize("---------------------------------------------------------------------------------------------------", getDieColor(white)))
-  fmt.Println(Colorize("===================================================================================================", getDieColor(white)))
-  fmt.Println("\n\n")
+ }
+ fmt.Println("\n\n")
 }
+
 // END  =========================================================
+
